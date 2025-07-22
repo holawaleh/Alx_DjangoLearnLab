7c74269149d61ed4f8b471d61c6d47d43e46273a
holawaleh <holawaleh@gmail.com>
2025-07-22 10:29:28 +0100

    TASK 2

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm  # ✅ <- THIS MUST BE HERE!
 from django.views.generic import DetailView
 
 from .models import Book, Library
+
 
 
 # --- Function-Based View ---
