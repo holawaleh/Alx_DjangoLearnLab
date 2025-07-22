cd4796966b5fc51c1fa1174422d9c39ba42cfa01
holawaleh <holawaleh@gmail.com>
2025-07-22 08:34:40 +0100

    error fixed

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
-from .models import Book
-from django.views.generic import DetailView
-from .models import Library
+from django.views.generic import DetailView  # ✅ CBV import
 
+from .models import Book, Library
+
+# Function-Based View
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-
+# Class-Based View
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
