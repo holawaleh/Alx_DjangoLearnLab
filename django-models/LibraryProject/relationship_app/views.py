f2430462d9e0bd6c0befe45a2045c9151d514976
holawaleh <holawaleh@gmail.com>
2025-08-08 07:50:29 +0100

    urls

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,62 +1,26 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate, logout
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from django.views.generic import DetailView
-from django.contrib import messages
+from django.shortcuts import render, get_object_or_404
+from .models import Author, Book, Library, Librarian
 
-from .models import Book, Library
+# Home view
+def home(request):
+    return render(request, 'relationship_app/home.html')
 
-# --- Function-Based View ---
-def list_books(request):
-    books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+# List all books
+def book_list(request):
+    books = Book.objects.select_related('author').all()
+    return render(request, 'relationship_app/book_list.html', {'books': books})
 
-# --- Class-Based View ---
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
+# Book detail
+def book_detail(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    return render(request, 'relationship_app/book_detail.html', {'book': book})
 
-# --- Auth Views ---
-def user_login(request):
-    if request.user.is_authenticated:
-        messages.info(request, "You are already logged in.")
-        return redirect('list_books')
+# List all libraries
+def library_list(request):
+    libraries = Library.objects.select_related('librarian').all()
+    return render(request, 'relationship_app/library_list.html', {'libraries': libraries})
 
-    if request.method == "POST":
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            messages.success(request, f"Welcome, {user.username}!")
-            return redirect('list_books')
-        else:
-            messages.error(request, "Invalid username or password.")
-    else:
-        form = AuthenticationForm()
-
-    return render(request, 'relationship_app/login.html', {'form': form})
-
-def user_logout(request):
-    logout(request)
-    messages.info(request, "You have been logged out.")
-    return render(request, 'relationship_app/logout.html')
-
-def register(request):
-    if request.user.is_authenticated:
-        messages.info(request, "You already have an account.")
-        return redirect('list_books')
-
-    if request.method == "POST":
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            messages.success(request, "Registration successful.")
-            return redirect('list_books')
-        else:
-            messages.error(request, "Please correct the errors below.")
-    else:
-        form = UserCreationForm()
-
-    return render(request, 'relationship_app/register.html', {'form': form})
+# Filter books by author name
+def books_by_author(request, author_name):
+    books = Book.objects.filter(author__name__icontains=author_name)
+    return render(request, 'relationship_app/book_list.html', {'books': books})
