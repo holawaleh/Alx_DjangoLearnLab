e306b42411fc3dbcffabda3b759ad70bab2ec803
holawaleh <holawaleh@gmail.com>
2025-08-29 08:55:34 +0100

    Implementing Advanced Features: Tagging and Search Functionality

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,3 +1,4 @@
+# blog/forms.py
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
@@ -12,7 +13,7 @@ class PostForm(forms.ModelForm):
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 10}),
-            'tags': TagWidget(attrs={'class': 'form-control', 'placeholder': 'Add tags...'}),
+            'tags': TagWidget(attrs={'class': 'form-control', 'placeholder': 'Add tags...'}),  # <-- TagWidget
         }
 
 
